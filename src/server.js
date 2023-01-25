3b82c958bbc22b7c4f045f4763da3d9b8211901b
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-01-25 14:47:07 +0000

    Server set up, Schema and seeding.
    Co-authored-by: Laura Keating <LauraK0@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const films = require("../routes/films.js");
+const films = require('../routes/films.js');
 // const add = require('../routes/add.js');
 
 const server = express();
 // const bodyParser = express.urlencoded({extended: false});
 
-server.get("/", films.get);
+server.get('/', films.get);
 // server.get('/new', add.get);
 // server.post('/new', bodyParser, add.post);
 
